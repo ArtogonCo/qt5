c778da0a093c01adf200cfcb9e79c6edab93779f
Ivan Kozlov <ik9559378ik@hotmail.com>
2015-07-27 19:51:38 +0300

    config bat-s added

---
+++ my_configure.bat
@@ -0,0 +1,11 @@
+
+set PATH=%CD%\bin;%PATH%
+set PATH=C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\;%PATH%
+set PATH=C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\;%PATH%
+set PATH=C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\;%PATH%
+set INCLUDE=C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\include\;C:\Program Files\Microsoft SDKs\Windows\v7.1\Include\
+set LIB=C:\Program Files\Microsoft SDKs\Windows\v7.1\Lib\;C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\lib\
+set QMAKESPEC=win32-msvc2010
+set DXSDK_DIR=C:\DirectXSDK\
+
+configure -developer-build -nomake examples -nomake tests
---
+++ my_make.bat
@@ -0,0 +1,12 @@
+set PATH=%CD%\bin;%PATH%
+set PATH=C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\;%PATH%
+set PATH=C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\;%PATH%
+set PATH=C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\;%PATH%
+set PATH=C:\Python27\;%PATH%
+rem set INCLUDE=C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\include\;C:\Program Files\Microsoft SDKs\Windows\v7.1\Include\;C:\ArtogonProjects\AngleProject\include\
+rem set LIB=C:\Program Files\Microsoft SDKs\Windows\v7.1\Lib\;C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\lib\;D:\ArtogonProjects\AngleProject\lib\Release\
+set INCLUDE=C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\include\;C:\Program Files\Microsoft SDKs\Windows\v7.1\Include\
+set LIB=C:\Program Files\Microsoft SDKs\Windows\v7.1\Lib\;C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\lib\
+set QMAKESPEC=win32-msvc2010
+
+nmake
