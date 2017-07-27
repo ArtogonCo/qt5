8f9207d21b314aa9bd45311b72f3becf249d1253
Ivan Kozlov <ik9559378ik@hotmail.com>
2017-07-27 14:43:58 +0300

    corrected my_configure.bat and my_make.bat

--- my_make.bat
+++ my_make.bat
@@ -1,6 +1,8 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86 8.1
 
-set QMAKESPEC=win32-msvc
+rem Please make sure to unset the QMAKESPEC, XQMAKESPEC, QMAKEPATH, and QMAKEFEATURES environment variables prior to building Qt.
+ 
+rem set QMAKESPEC=win32-msvc
 set QMAKE_MSC_VER=2015
 set DXSDK_DIR=C:\DirectXSDK\
 
