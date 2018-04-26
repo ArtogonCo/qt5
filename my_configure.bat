fc99712efe829a791682ecb913333e89f6df67e1
Ivan <100kozlov@inbox.ru>
2018-04-26 15:31:10 +0300

    Submodules url is git://code.qt.io/qt now

--- my_configure.bat
+++ my_configure.bat
@@ -1,14 +1,5 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86 8.1
+rem call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86 8.1
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64 10.0.15063.0
 
-rem Please make sure to unset the QMAKESPEC, XQMAKESPEC, QMAKEPATH, and QMAKEFEATURES environment variables prior to building Qt.
 
-set QMAKESPEC=
-set XQMAKESPEC=
-set QMAKEPATH=
-set QMAKEFEATURES=
-
-rem set QMAKESPEC=win32-msvc
-set QMAKE_MSC_VER=2015
-set DXSDK_DIR=C:\DirectXSDK\
-
-configure -developer-build -nomake examples -nomake tests -platform win32-msvc -opensource -confirm-license
+configure -developer-build -nomake examples -nomake tests -opensource -confirm-license -skip qtwebkit -skip qtwebkit-examples -skip qtwebengine 
--- my_make.bat
+++ my_make.bat
@@ -1,12 +1,7 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86 8.1
+rem call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86 8.1
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64 10.0.15063.0
 
-rem Please make sure to unset the QMAKESPEC, XQMAKESPEC, QMAKEPATH, and QMAKEFEATURES environment variables prior to building Qt.
- 
-rem set QMAKESPEC=win32-msvc
-set QMAKE_MSC_VER=2015
-set DXSDK_DIR=C:\DirectXSDK\
 
-set PATH=%CD%\bin;%PATH%
 set PATH=C:\Python27\;%PATH%
 
 nmake
